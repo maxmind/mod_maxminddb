810c058846f3b37a2c0e57f253de9107313ecddc
Boris Zentner <bzentner@maxmind.com>
2014-01-14 16:41:16 +0100

    Add missing datatypes

--- mod_maxminddb.c
+++ mod_maxminddb.c
@@ -34,6 +34,7 @@
 #include "maxminddb.h"
 #include <string.h>
 #include <alloca.h>
+#include <inttypes.h>
 
 #if defined (MAXMINDDB_DEBUG)
 #define INFO(server_rec, ...)                                            \
@@ -495,6 +496,29 @@ static void set_user_env(request_rec * r, maxminddb_server_config * mmsrvcfg,
                     case MMDB_DATA_TYPE_INT32:
                         len = asprintf(&value, "%d", result.int32);
                         break;
+                     case MMDB_DATA_TYPE_UINT64:
+                        len = asprintf(&value, "%" PRIu64, result.uint64);
+                        break;
+                    case MMDB_DATA_TYPE_INT64:
+                        len = asprintf(&value, "%" PRId64, result.int64);
+                        break;
+                    case MMDB_DATA_TYPE_UINT128:
+#if MMDB_UINT128_IS_BYTE_ARRAY
+                        uint8_t* p = (uint8_t*)result.uint128;
+                        len = asprintf(&value, "%02x%02x%02x%02x"
+                                               "%02x%02x%02x%02x"
+                                               "%02x%02x%02x%02x"
+                                               "%02x%02x%02x%02x",
+                                       p[0], p[1], p[2], p[3],
+                                       p[4], p[5], p[6], p[7],
+                                       p[8], p[9], p[10], p[11],
+                                       p[12], p[13], p[14], p[15]);
+#else
+                        mmdb_uint128_t v = result.uint128;
+                        len = asprintf(&value, "%016" PRIx64 "%016" PRIx64, (uint64_t)(v >> 64), (uint64_t)v);
+#endif
+                        break;
+
                     default:
                         len = asprintf(&value, "Unsupported");
                         break;
