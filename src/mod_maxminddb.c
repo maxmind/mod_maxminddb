30a2be6d8d68f3ee445de61e9a9d68da51599227
Jonah H. Harris <jonah.harris@meetme.com>
2016-09-01 11:24:37 -0400

    Add ability for override the name of the MMDB_ADDR IP lookup environment variable.

--- src/mod_maxminddb.c
+++ src/mod_maxminddb.c
@@ -55,6 +55,7 @@ APLOG_USE_MODULE(maxminddb);
 typedef struct maxminddb_config {
     apr_hash_t *databases;
     apr_hash_t *lookups;
+    char *mmdb_addr_env_var;
     int enabled;
 } maxminddb_config;
 
@@ -96,6 +97,11 @@ static const command_rec maxminddb_directives[] = {
                  NULL,
                  OR_ALL,
                  "Turn on mod_maxminddb"),
+    AP_INIT_TAKE1("MaxMindDBAddrEnv",
+                  set_maxminddb_addr_env,
+                  NULL,
+                  OR_ALL,
+                  "Set the queried IP address env var (default MMDB_ADDR)"),
     AP_INIT_TAKE2("MaxMindDBFile",
                   set_maxminddb_filename,
                   NULL,
@@ -147,6 +153,7 @@ static void *create_config(apr_pool_t *pool)
 
     conf->databases = apr_hash_make(pool);
     conf->lookups = apr_hash_make(pool);
+    conf->mmdb_addr_env_var = apr_pstrdup(pool, "MMDB_ADDR");
 
     /* We use -1 for off but not set */
     conf->enabled = -1;
@@ -273,6 +280,18 @@ static const char *set_maxminddb_env(cmd_parms *cmd, void *dir_config,
     return NULL;
 }
 
+static const char *set_maxminddb_addr_env(cmd_parms *cmd, void *dir_config,
+                                     const char *env)
+{
+    maxminddb_config *conf = get_config(cmd, dir_config);
+
+    INFO(cmd->server, "set_maxminddb_addr_env (server) %s", env);
+
+    conf->mmdb_addr_env_var = apr_pstrdup(cmd->pool, env);
+
+    return NULL;
+}
+
 static int export_env_for_server(request_rec *r)
 {
     INFO(r->server, "maxminddb_per_server ( enabled )");
@@ -298,7 +317,7 @@ static int export_env(request_rec *r, maxminddb_config *conf)
     if (NULL == ip_address) {
         return DECLINED;
     }
-    apr_table_set(r->subprocess_env, "MMDB_ADDR", ip_address);
+    apr_table_set(r->subprocess_env, conf->mmdb_addr_env_var, ip_address);
 
     for (apr_hash_index_t *db_index = apr_hash_first(r->pool, conf->databases);
          db_index; db_index = apr_hash_next(db_index)) {
